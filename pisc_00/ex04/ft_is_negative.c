111e0d1967cc1ce83a086b618da81f8fb90ab486
DemonLaSushi <104307034+DemonLaSushi@users.noreply.github.com>
2022-10-25 22:26:29 +0200

    Add files via upload

--- pisc_00/ex04/ft_is_negative.c
+++ pisc_00/ex04/ft_is_negative.c
@@ -6,23 +6,21 @@
 /*   By: lpryce <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 19:40:58 by lpryce            #+#    #+#             */
-/*   Updated: 2022/10/25 20:10:51 by lpryce           ###   ########.fr       */
+/*   Updated: 2022/10/25 21:47:51 by lpryce           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
 void ft_is_negative(int n){
-	int n;
-
 	if(n >= 0)
 	{
-	write(1, 'P', 1);
+	write(1,"P",1);
 	}
-	else(n <= 0){
-	write(1, 'N', 1);
+	else
+	{
+	write(1,"N",1);
 	}
+
 }
-int main(){
-ft_is_negative();
-}
+
