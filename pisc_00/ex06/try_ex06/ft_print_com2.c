c01d42f8b8a32e9cbb781af3ea469280956ce903
DemonLaSushi <104307034+DemonLaSushi@users.noreply.github.com>
2022-10-27 17:38:56 +0200

    Add files via upload

--- pisc_00/ex06/try_ex06/ft_print_com2.c
+++ pisc_00/ex06/try_ex06/ft_print_com2.c
@@ -1,56 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_combo2.c                                  :+:      :+:    :+:   */
+/*   ft_print_com2.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lpryce <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/27 11:59:49 by lpryce            #+#    #+#             */
-/*   Updated: 2022/10/27 16:51:31 by lpryce           ###   ########.fr       */
+/*   Created: 2022/10/27 17:03:13 by lpryce            #+#    #+#             */
+/*   Updated: 2022/10/27 17:38:36 by lpryce           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_print_combo2(void){
+void ft_print_com2(void)
 
 	int a;
 	int b;
-	int c;
-	int d;
 
-	a = '1';
-	b = '0';
-	c = '0';
-	d = '0';
+	a = "01"
+	b = "00"
 
-	while(d <= '9')
-	{
-		//d = c+1;
-		while(c <= '9')
-		{
-			//c = b+1;
-			while(b <= '9')
-			{
-				b = a+1;
-				while(a<='9')
-				{
-					write(1, &d, 1);
-					write(1, &c, 1);
-					write(1, " ", 1);
-					write(1, &b, 1);
-					write(1, &a, 1);
-					write(1, ", ", 2);
-
-					a++;
-				}
-				b++;
-			}
-			c++;
+	while(a++ < 98){
+		
+		while(a ="99"){
+			a - "98";
+			b++;
 		}
-		d++;
+	write(1, &a, 2);
+	write(1," ", 1);
+	write(1, &b, 2);
+	write(1,", ", 2);
 	}
-}
-int main(){
-	ft_print_combo2();
-}
+int main (
