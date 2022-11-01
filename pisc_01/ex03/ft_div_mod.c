ac20b16b3137782cc2482020dcb3de2be1e35969
DemonLaSushi <104307034+DemonLaSushi@users.noreply.github.com>
2022-11-01 17:26:43 +0100

    Add files via upload

--- pisc_01/ex03/ft_div_mod.c
+++ pisc_01/ex03/ft_div_mod.c
@@ -6,13 +6,15 @@
 /*   By: lpryce <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/30 21:35:56 by lpryce            #+#    #+#             */
-/*   Updated: 2022/10/30 21:38:50 by lpryce           ###   ########.fr       */
+/*   Updated: 2022/11/01 15:57:09 by lpryce           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
 void	ft_div_mod(int a, int b, int *div, int *mod)
 {
-
+	*div = a / b;
+	*mod = a % b;
 }
